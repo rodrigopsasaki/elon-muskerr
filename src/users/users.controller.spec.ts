acfb7db6db3bda57f9703ae6b0d08fe06f186e66
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2022-11-12 19:39:14 -0300

    test(users) adding in-memory database and service mocks

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersServiceMock: Partial<UsersService>;
 
   beforeEach(async () => {
+    usersServiceMock = {
+      createUser: (username: string) => {
+        return Promise.resolve();
+      },
+      list: () => {
+        return Promise.resolve([
+          { id: 1, username: 'elonmusk', createdAt: new Date() },
+        ]);
+      },
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: usersServiceMock }],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
